3a8f00899f050cd5c00abc812d449ffafb8be452
Adam Czajkowski <dsw55987@student.dsw.edu.pl>
2025-05-17 20:33:43 +0200

    Dodano logs

--- zad1.sh
+++ zad1.sh
@@ -4,6 +4,13 @@ case "$1" in
     --date | -d)
         date +"%Y-%m-%d"
         ;;
+    --logs | -l)
+    count=${2:-100}
+    for i in $(seq 1 $count); do
+        echo "log$i.txt | skrypt.sh | $(date)" > log$i.txt
+    done
+    ;;
+
     *)
         echo "Nieznane polecenie uzyj --help."
         ;;
