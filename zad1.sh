40ce77f3e64b7a9cf34c59b2749bf0245752a42f
Adam Czajkowski <dsw55987@student.dsw.edu.pl>
2025-05-17 20:27:21 +0200

    nowa flaga --date

--- zad1.sh
+++ zad1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Funkcje w trakcie implementacji"
+case "$1" in
+    --date | -d)
+        date +"%Y-%m-%d"
+        ;;
+    *)
+        echo "Nieznane polecenie uzyj --help."
+        ;;
+esac
 
